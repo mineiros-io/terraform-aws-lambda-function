1478ad438ac672002beef0f7328116013733a146
Soren Martius <soeren.martius@gmail.com>
2020-07-14 12:19:41 +0200

    Align test structure with latest template requirements

--- test/unit_minimal_test.go
+++ test/unit_minimal_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLambdaS3(t *testing.T) {
+func TestUnitMinimal(t *testing.T) {
 	t.Parallel()
 
 	functionName := strings.ToLower(fmt.Sprintf("lambda-test-%s", random.UniqueId()))
@@ -23,8 +23,6 @@ func TestLambdaS3(t *testing.T) {
 	memorySize := float64(128)
 	timeout := float64(3)
 
-	s3BucketPrefix := "mineiros-lambda-test-"
-
 	moduleTags := map[string]string{
 		"Name":        functionName,
 		"Environment": "Dev",
@@ -32,17 +30,16 @@ func TestLambdaS3(t *testing.T) {
 
 	terraformOptions := &terraform.Options{
 		// The path to where the Terraform code is located
-		TerraformDir: "./lambda-s3",
+		TerraformDir: "./unit-minimal",
 		Vars: map[string]interface{}{
-			"function_name":    functionName,
-			"description":      description,
-			"s3_bucket_prefix": s3BucketPrefix,
-			"handler":          handler,
-			"runtime":          runtime,
-			"publish":          publish,
-			"memory_size":      memorySize,
-			"timeout":          timeout,
-			"module_tags":      moduleTags,
+			"function_name": functionName,
+			"description":   description,
+			"handler":       handler,
+			"runtime":       runtime,
+			"publish":       publish,
+			"memory_size":   memorySize,
+			"timeout":       timeout,
+			"module_tags":   moduleTags,
 		},
 		Upgrade: true,
 	}
@@ -50,8 +47,8 @@ func TestLambdaS3(t *testing.T) {
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-	terraform.InitAndApply(t, terraformOptions)
+	terraform.InitAndPlan(t, terraformOptions)
+	terraform.ApplyAndIdempotent(t, terraformOptions)
 
 	outputs := terraform.OutputAll(t, terraformOptions)
 	functionOutputs := outputs["all"].(map[string]interface{})["function"].(map[string]interface{})
