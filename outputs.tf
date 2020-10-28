cad8af842bc2bfc28e0542d0a9acffcba0e3df56
Marius Tolzmann <m@rius.berlin>
2020-10-28 01:10:25 +0100

    test

--- outputs.tf
+++ outputs.tf
@@ -25,35 +25,35 @@ output "permissions" {
 # OUTPUT ALL INPUT VARIABLES
 # ----------------------------------------------------------------------------------------------------------------------
 
-output "module_inputs" {
-  description = "A map of all module arguments. Omitted optional arguments will be represented with their actual defaults."
-  value = {
-    aliases                        = local.aliases
-    dead_letter_config_target_arn  = var.dead_letter_config_target_arn
-    description                    = var.description
-    environment_variables          = var.environment_variables
-    filename                       = var.filename
-    function_tags                  = var.function_tags
-    function_name                  = var.function_name
-    handler                        = var.handler
-    kms_key_arn                    = var.kms_key_arn
-    layer_arns                     = var.layer_arns
-    memory_size                    = var.memory_size
-    permissions                    = values(local.permissions)
-    publish                        = var.publish
-    reserved_concurrent_executions = var.reserved_concurrent_executions
-    runtime                        = var.runtime
-    role_arn                       = var.role_arn
-    s3_bucket                      = local.s3_bucket
-    s3_key                         = local.s3_key
-    s3_object_version              = local.s3_object_version
-    source_code_hash               = local.source_code_hash
-    timeout                        = var.timeout
-    tracing_mode                   = var.tracing_mode
-    vpc_subnet_ids                 = var.vpc_subnet_ids
-    vpc_security_group_ids         = var.vpc_security_group_ids
-  }
-}
+# output "module_inputs" {
+#   description = "A map of all module arguments. Omitted optional arguments will be represented with their actual defaults."
+#   value = {
+#     aliases                        = local.aliases
+#     dead_letter_config_target_arn  = var.dead_letter_config_target_arn
+#     description                    = var.description
+#     environment_variables          = var.environment_variables
+#     filename                       = var.filename
+#     function_tags                  = var.function_tags
+#     function_name                  = var.function_name
+#     handler                        = var.handler
+#     kms_key_arn                    = var.kms_key_arn
+#     layer_arns                     = var.layer_arns
+#     memory_size                    = var.memory_size
+#     permissions                    = values(local.permissions)
+#     publish                        = var.publish
+#     reserved_concurrent_executions = var.reserved_concurrent_executions
+#     runtime                        = var.runtime
+#     role_arn                       = var.role_arn
+#     s3_bucket                      = local.s3_bucket
+#     s3_key                         = local.s3_key
+#     s3_object_version              = local.s3_object_version
+#     source_code_hash               = local.source_code_hash
+#     timeout                        = var.timeout
+#     tracing_mode                   = var.tracing_mode
+#     vpc_subnet_ids                 = var.vpc_subnet_ids
+#     vpc_security_group_ids         = var.vpc_security_group_ids
+#   }
+# }
 
 # ----------------------------------------------------------------------------------------------------------------------
 # OUTPUT MODULE CONFIGURATION
