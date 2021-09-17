028437b36064a7946d6011c467fc8175b2a8deb8
Soren Martius <soeren.martius@gmail.com>
2021-09-17 10:03:06 +0100

    feat: upgrade mineiros module versions to latest versions

--- examples/s3-complete-example/main.tf
+++ examples/s3-complete-example/main.tf
@@ -127,7 +127,7 @@ resource "aws_sns_topic_subscription" "lambda" {
 
 module "iam_role" {
   source  = "mineiros-io/iam-role/aws"
-  version = "~> 0.5.0"
+  version = "~> 0.6.0"
 
   name = "mineiros-s3-lambda-example"
 
